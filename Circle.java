5c251fd56e493d1cb812b3f91666e24e217b093e
RichardAlcalaC <jheranium@gmail.com>
2017-06-09 10:07:31 -0700

    Initial sharing of project

---
+++ Circle.java
@@ -0,0 +1,67 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Write a description of class Circle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circle
+{
+    private int radius;
+    private int x;
+    private int y;
+    private Color color;
+    private boolean changeAspect;
+    
+    public Circle(int radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+    
+    public Circle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        color = getNextColor();
+        radius = 25;
+    }
+    
+    public void draw(Graphics g, int x, int y) {
+        this.draw(g);
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void draw(Graphics g) {
+        if (changeAspect) {
+            color = getNextColor();
+            changeAspect = false;
+            radius = (int)(200 * Math.random());
+        }
+        g.setColor(color);
+        g.fillOval(x - radius , y - radius, radius * 2, radius * 2);
+    }
+    
+    public void clickAt(int x, int y) {
+        changeAspect = isInsideCircle(x, y);
+    }
+    
+    
+    
+    private Color getNextColor() {
+        int red = (int) (255 * Math.random());
+        int green = (int) (255 * Math.random());
+        int blue = (int) (255 * Math.random());
+        
+        return new Color(red, green, blue);
+    }
+    
+    public boolean isInsideCircle(int x, int y) {
+        int xCenter = this.x;
+        int yCenter = this.y;
+        double d = Math.hypot(yCenter - y, xCenter - x);
+        return d <= radius;
+    }
+}
---
+++ DrawingPanel.java
@@ -0,0 +1,63 @@
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Collection;
+import java.util.Vector;
+
+/**
+ * Write a description of class DrawingPanel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DrawingPanel extends JPanel
+{
+    //private Circle circle;
+    private Collection<Circle> circles;
+    
+    public DrawingPanel() {
+        
+        //circle = new Circle(25, Color.yellow);
+        circles = new Vector<Circle>();
+        
+        setBackground(Color.GRAY);
+        
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                if (!clickIsInsideAnyCircle(event)) {
+                    addNewCircle(event);
+                }
+                repaint();
+            }
+        });
+    }
+    
+    private boolean clickIsInsideAnyCircle(MouseEvent event) {
+        boolean isInsideAnyCircle = false;
+        for (Circle circle: circles) {
+            circle.clickAt(event.getX(), event.getY());
+            if (circle.isInsideCircle(event.getX(), event.getY())) {
+                isInsideAnyCircle = true;
+            }            
+        }
+        return isInsideAnyCircle;
+    }
+    
+    private void addNewCircle(MouseEvent event) {
+        circles.add(new Circle(event.getX(), event.getY()));
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        for (Circle circle: circles) {
+            circle.draw(g);
+        }
+    }
+}
